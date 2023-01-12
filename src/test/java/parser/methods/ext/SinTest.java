0c7177e85e60f6e7ba5eeb88a3daa9dd988ad67f
Jiri Vanek <jvanek@redhat.com>
2023-01-12 17:35:52 +0100

    Exempalr test how to fix functions in meantime without changing ParserNG

--- src/test/java/parser/methods/ext/SinTest.java
+++ src/test/java/parser/methods/ext/SinTest.java
@@ -3,7 +3,14 @@ package parser.methods.ext;
 import logic.DRG_MODE;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 import parser.MathExpression;
+import parser.TYPE;
+import parser.methods.BasicNumericalMethod;
+import parser.methods.Declarations;
 
 public class SinTest {
 
@@ -137,5 +144,54 @@ public class SinTest {
         } finally {
             System.setProperty(DRG_MODE.DEG_MODE_VARIABLE, "RAD");
         }
+    }
+
+    @Test
+    void hardcodedFunctionCanBeOverwritten() {
+        MathExpression me;
+        me = new MathExpression("prod(1+1,2+2)");
+        Assertions.assertEquals("8.0", me.solve());
+        me = new MathExpression("prod(1+1)");
+        Assertions.assertEquals("SYNTAX ERROR", me.solve());
+        BasicNumericalMethod override = new BasicNumericalMethod() {
+            @Override
+            public String solve(List<String> tokens) {
+                List<BigDecimal> numericalTokens = Utils.evaluateSingleToken(tokens);
+                BigDecimal result = new BigDecimal("1");
+                for(BigDecimal numericalToken: numericalTokens){
+                    result = result.multiply(numericalToken);
+                }
+                return result.toString();
+            }
+
+            @Override
+            public String getHelp() {
+                return "fake prod";
+            }
+
+            @Override
+            public String getName() {
+                return "prod";
+            }
+
+            @Override
+            public String getType() {
+                return TYPE.NUMBER.toString();
+            }
+        };
+        int before = Declarations.getInbuiltMethods().length;
+        Declarations.registerBasicNumericalMethod(override);
+        int during = Declarations.getInbuiltMethods().length;
+        try {
+            me = new MathExpression("prod(1+1)");
+            Assertions.assertEquals("2.0", me.solve());
+        } finally {
+            Declarations.unregisterBasicNumericalMethod(override.getClass());
+        }
+        int after = Declarations.getInbuiltMethods().length;
+        me = new MathExpression("prod(1+1)");
+        Assertions.assertEquals("SYNTAX ERROR", me.solve());
+        Assertions.assertEquals(before, after);
+        Assertions.assertEquals(before+1, during);
     }
 }
